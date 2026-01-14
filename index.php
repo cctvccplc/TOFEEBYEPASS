6175529cabe054a40b93ecd09c0352c04da506b4
cctvccplc <cctvccplc@gmail.com>
2026-01-15 02:09:39 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Premium IPTV Player</title>
+    <link href="https://vjs.zencdn.net/7.20.3/video-js.css" rel="stylesheet" />
+    <style>body{background:#000; color:#fff; text-align:center; font-family:sans-serif;}</style>
+</head>
+<body>
+    <h1>Toffee Live Web Player</h1>
+    <video id="player" class="video-js vjs-default-skin vjs-big-play-centered" controls width="800" height="450">
+        <source src="proxy.php?url=https://bldcmprod-cdn.toffeelive.com/cdn/live/sony_sports_1_hd/playlist.m3u8" type="application/x-mpegURL">
+    </video>
+    <script src="https://vjs.zencdn.net/7.20.3/video.min.js"></script>
+</body>
+</html>
